2a89e342fe7f3ac0ebee175e62aaf6702c1fcc05
Toby Henderson <toby.henderson@huddle.com>
2015-05-08 18:04:43 +0100

    almost all building 4.5 dnx4.5 building, just dnxcore5.0 errors due to system.configuration

--- Brighter/paramore.brighter.commandprocessor/ConfigurationException.cs
+++ Brighter/paramore.brighter.commandprocessor/ConfigurationException.cs
@@ -69,15 +69,5 @@ namespace paramore.brighter.commandprocessor
             : base(message, innerException)
         {
         }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="T:System.Exception" /> class with serialized data.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
-        protected ConfigurationException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
     }
 }
